12a30981d5ab05a812b504e5b5ee46b0ac2d6977
Alex <alexg@tyrexsoftware.com>
2015-06-18 00:06:38 +0300

    v folder

---
+++ vendor/kartik-v/yii2-date-range/MomentAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @package yii2-date-range
+ * @version 1.5.0
+ */
+
+namespace kartik\daterange;
+
+/**
+ * MomentAsset bundle for \kartik\daterange\DateRangePicker.
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class MomentAsset extends \kartik\base\AssetBundle
+{
+    public $jsOptions = ['position' => \yii\web\View::POS_HEAD];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('js', ['js/moment']);
+        parent::init();
+    }
+
+}
