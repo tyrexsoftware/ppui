279773243300b8b954d939d892d0fb09e4afaae3
AlexG <snobist@gmail.com>
2015-06-17 16:01:07 +0300

    db.php updated

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ppdb',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
