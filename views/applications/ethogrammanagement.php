9432e424d85faf74aeb7b7030afbaac1755d76c6
Alex <alexg@tyrexsoftware.com>
2015-06-26 08:54:36 +0300

    restoring libs

--- views/applications/ethogrammanagement.php
+++ views/applications/ethogrammanagement.php
@@ -1,1 +1,27 @@
 <?php
+        $this->registerJsFile('@web/js/ethogram.js');
+?>
+
+<div class="manage_animals">
+    <div>
+        <div class="m_title_wrapper">
+            <div class="m_title">Aggressive</div>
+            <div class="m_title_edit">
+                <a class="collapseL" href="#"></a>
+                <a class="delete" href="#"></a>
+                <a class="edit" href="#"></a>
+            </div>
+        </div>
+        <div class="m_slide">
+            <div class="m_text"><span>Touch</span><a class="delete" href="#"></a><a class="edit" href="#"></a></div>
+            <div class="m_text"><span>Touch</span><a class="delete" href="#"></a><a class="edit" href="#"></a></div>
+            <div id="m_action">Add new action</div>
+        </div>
+    </div>
+    
+    <hr class="sep"/>
+
+    <div class="m_type_text"><input placeholder="Type a name of behavior" type="text"></div>
+    <div class="m_buttons"><button class="m_add_action">Add new action</button></div>
+</div>
+<script>$(ehtogram)</script>
---
+++ web/js/ethogram.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */;
+
+(function ($) {
+
+    $.fn.ehtogram = function () {
+        return $('#m_action').click(function()(console.log('kuku')))
+        addBehavior = function () {
+
+        }
+    }
+}(jQuery));
+
