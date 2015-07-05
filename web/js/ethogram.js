bb7535f18907afe179746e973a3eed68da042a48
Alex <alexg@tyrexsoftware.com>
2015-07-05 23:22:39 +0300

    Even More changes to ethogram

--- web/js/ethogram.js
+++ web/js/ethogram.js
@@ -5,7 +5,7 @@
  */;
 
 
-(function($) {
+(function ($) {
     var buttonClass = 'm_action';
     var containerclass = 'ma_block';
     var boxclass = 'm_slide';
@@ -13,6 +13,7 @@
 
 
     var subjectInput = {type: '<input/>', values: {type: 'text', class: 'subjectInput'}};
+    var behaviorInput = {type: '<input/>', values: {type: 'text', class: 'newInput'}};
 
     var collapseButton = {type: '<a/>', values: {class: 'collapseL'}};
     var deleteButton = {type: '<a/>', values: {class: 'delete fa-trash-o'}};
@@ -26,23 +27,26 @@
     var ethogram;
 
     var method = {
-        initialize: function(params) {
+        initialize: function (params) {
 
             self.ethogram = $(this);
 
             $(".manage_blocks").sortable({
                 items: '.ma_block:not(.inactive)',
                 handle: '.drag',
-                start: function(event, ui) {
+                start: function (event, ui) {
                     prevPagesOrder = $(this).sortable('toArray');
                 },
-                stop: function(event, ui) {
-                             
+                stop: function (event, ui) {
+
                     var currentOrder = $(this).sortable('toArray');
                     var first = ui.item[0].id;
                     var second = currentOrder[prevPagesOrder.indexOf(first)];
-                    method.saveContainer($('#'+first));
-                    method.saveContainer($('#'+second));
+                    if (first == second) {
+                        return;
+                    }
+                    method.saveContainer($('#' + first));
+                    method.saveContainer($('#' + second));
                 }
 
             });
@@ -52,7 +56,7 @@
 
             var inactive = $('<div/>', {class: buttonClass + ' inactive'})
                     .text('Add new section')
-                    .on('click', function() {
+                    .on('click', function () {
                         method.addContainer('new', {name: '', values: {}})
                         $('.subjectInput').last().focus();
 //                        $('.success.pendning').last().on('click', function(event)
@@ -71,7 +75,7 @@
 
             return this;
         },
-        buildEhtogram: function(data) {
+        buildEhtogram: function (data) {
 
 
 
@@ -80,17 +84,17 @@
 
 
         },
-        addContainer: function(type, containervalues) {
+        addContainer: function (type, containervalues) {
 
             var buttonsRow = $('<div/>', {class: 'm_title_edit'})
             if (type == 'new') {
 
                 $(buttonsRow)
-                        .prepend(method.createElement(successPendingButton).on('click', function(event) {
+                        .prepend(method.createElement(successPendingButton).on('click', function (event) {
                             method.saveContainer(method.getContainer(event))
                         }))
                         .prepend($(method.createElement(deleteButton)
-                                .on('click', function(event) {
+                                .on('click', function (event) {
                                     method.askQuestion(method.deleteContainer, method.getContainer(event))
                                 })
                                 ))
@@ -98,11 +102,11 @@
             } else {
                 $(buttonsRow).prepend(method.createElement(dragButton))
 //                    .prepend(successButton)
-                        .prepend(method.createElement(editButton).on('click', function(event) {
+                        .prepend(method.createElement(editButton).on('click', function (event) {
                             method.editContainer(method.getContainer(event))
                         }))
                         .prepend($(method.createElement(deleteButton)
-                                .on('click', function(event) {
+                                .on('click', function (event) {
                                     method.askQuestion(method.deleteContainer, method.getContainer(event))
                                 })
                                 ))
@@ -113,79 +117,102 @@
             var containerelement = $('<div/>', {class: containerclass, id: containervalues.id})
                     .insertBefore($('.inactive'));
 
+            var title = method.createTitle($(method.createElement(subjectInput)
+                    .on('focusin', function (event) {
+                        method.editContainer(method.getContainer(event))
+                    }))
+                    .on('keypress', function (event) {
+                        if (event.which == 13) {
+                            method.saveContainer(method.getContainer(event))
+                        }
+                    })
+                    .val(containervalues.name)
+                    .attr('placeholder', containervalues.name)).append(buttonsRow);
+
+            $(title).prependTo(containerelement);
+
+            var buttonsContainer = $('<div/>', {class: boxclass}).sortable({
+                connectWith: "." + boxclass,
+                items: 'div:not(.m_action)',
+                handle: '.drag'
+            });
+
+
+
             //add button and make tiles sortable;
 
             var newActionButton = $('<div/>', {class: buttonClass})
                     .text('New Behavior')
-                    .on('click', function() {
+                    .on('click', function () {
                         $('<div/>', {class: textwrapperclass}).insertBefore($(this))
                                 .prepend($('<input>', {type: 'text', class: 'newInput'}))
                                 .prepend(method.createElement(successPendingButton))
                                 .prepend($(method.createElement(deleteButton)
-                                        .on('click', function(event) {
+                                        .on('click', function (event) {
                                             method.askQuestion(method.deleteBehavior, method.getContainer(event))
                                         })
                                         ));
 
                     });
 
-            //createa a button that adds an empty text field
-            newActionButton.prependTo($('<div/>', {class: boxclass}).sortable({
-                connectWith: "." + boxclass,
-                items: 'div:not(.m_action)'}).prependTo(containerelement));
-
-
-            var title = method.createTitle($(method.createElement(subjectInput).on('focusin', function(event) {
-                method.editContainer(method.getContainer(event))
-            }))
-                    .val(containervalues.name)
-                    .attr('placeholder', containervalues.name)).append(buttonsRow);
-
-            $(title).prependTo(containerelement);
-
             if ($(containervalues.values).size() > 1) {
-                $.each(containervalues.values, function(i, value) {
-                    method.addBehavior(value, containerelement)
+                $.each(containervalues.values, function (i, value) {
+
+                    $(buttonsContainer).prepend(method.addBehavior(value));
                 })
             }
+            $(buttonsContainer).append(newActionButton);
+            $(containerelement).append(buttonsContainer);
+
+
             return this;
 
         },
-        createElement: function(elementObject) {
+        createElement: function (elementObject) {
             return $(elementObject.type, elementObject.values);
         },
-        addBehavior: function(behavior, containerelement) {
+        addBehavior: function (behavorvalues) {
 
-            var deleteButton = $('<a/>', {class: 'delete'}).on('click', function() {
+            if (typeof behavorvalues.name === 'undefined') {
+                var name = '';
+                var id = '';
+                var buttonsRow = $(method.createElement(successPendingButton))
+                        .after(method.createElement(deleteButton));
+            } else {
+                var name = behavorvalues.name;
+                var id = behavorvalues.id;
 
-            });
-            var editButton = $('<a/>', {class: 'edit'}).on('click', function() {
-                console.log(behavior.id)
-            });
-            var buttonWrapper = $('<div/>', {class: textwrapperclass})
-                    .prependTo($('.' + boxclass, containerelement));
-            $('<input/>', {type: 'text', placeholder: behavior.name, value: behavior.name, class: 'newInput'}).
-                    prependTo(buttonWrapper)
-                    .after(editButton)
-                    .after(deleteButton);
+                var buttonsRow = $(method.createElement(deleteButton))
+                        .add(method.createElement(editButton).on('click',
+                                function (event) {
+                                    method.editBehavior(method.getBehaviorsBox(event))
+                                }
+                        ))
+                        .add(method.createElement(dragButton));
+            }
 
-            return this;
+            var behavior = $('<div/>', {class: textwrapperclass, id: id})
+                    .prepend(method.createElement(behaviorInput)
+                            .val(name)
+                            .attr('placeholder', name)).append(buttonsRow);
 
+
+            return behavior;
         },
-        createTitle: function(inputField) {
+        createTitle: function (inputField) {
 
             return $('<div/>', {class: 'm_title_wrapper'})
                     .prepend($('<div/>', {class: 'm_title'})
                             .prepend(inputField))
         },
-        editContainer: function(container) {
+        editContainer: function (container) {
 
             $('.m_title_edit', container).empty()
-                    .prepend(method.createElement(successPendingButton).on('click', function(event) {
+                    .prepend(method.createElement(successPendingButton).on('click', function (event) {
                         method.saveContainer(method.getContainer(event))
                     }))
                     .prepend($(method.createElement(deleteButton)
-                            .on('click', function(event) {
+                            .on('click', function (event) {
                                 method.askQuestion(method.deleteContainer, method.getContainer(event))
                             })
                             ));
@@ -194,7 +221,7 @@
 
 
         },
-        saveContainer: function(container) {
+        saveContainer: function (container) {
 
             var id = '';
             if (typeof $(container).attr('id') !== "undefined") {
@@ -207,84 +234,97 @@
 
 
             $.ajax({method: "POST", url: 'ethogramcontainer', data: {subject: value, position: $(container).index(), container_id: id}})
-                    .done(function(msg) {
+                    .done(function (msg) {
                         $(container).attr('id', msg.data.container_id);
                         $('.subjectInput', container).blur();
 
                         $('.m_title_edit', container)
                                 .prepend(method.createElement(dragButton))
-                                .prepend(method.createElement(editButton).on('click', function(event) {
+                                .prepend(method.createElement(editButton).on('click', function (event) {
                                     method.editContainer(method.getContainer(event))
                                 }))
                                 .prepend(method.createElement(deleteButton)
-                                        .on('click', function(event) {
+                                        .on('click', function (event) {
                                             method.askQuestion(method.deleteContainer, event)
                                         }))
 
                                 .prepend(method.createElement(collapseButton));
 
                         $('.loadingIcon', container)
-                                .fadeOut('slow', function() {
+                                .fadeOut('slow', function () {
                                     $(this).before(method.createElement(successOkButton));
                                     $('.success.ok').fadeOut(1500);
                                 });
                     });
 
         },
-        askQuestion: function(eventFunc, event) {
+        editBehavior: function (behaviorsBox) {
+            
+            console.log(behaviorsBox);
+
+            //console.log($('#' + id));
+            //$('#' + id).focus();
+
+        },
+        askQuestion: function (eventFunc, event) {
 
             $('<div></div>', {class: 'closePopup'}).appendTo('body')
                     .html('<div><h6>Are you sure you want to delete container?</h6></div>')
                     .dialog({
                         modal: true,
                         dialogClass: 'closePopup',
-                        title: 'Delete message',
+                        title: 'Delete',
                         zIndex: 10000,
                         autoOpen: true,
                         width: 'auto',
                         resizable: false,
                         buttons: {
-                            Yes: function() {
+                            Yes: function () {
                                 eventFunc(event);
                                 $(this).dialog("close");
                             },
-                            No: function() {
+                            No: function () {
                                 $(this).dialog("close");
                             }
                         },
-                        close: function(event, ui) {
+                        close: function (event, ui) {
                             $(this).remove();
                         }
                     });
         },
-        deleteContainer: function(container) {
+        deleteContainer: function (container) {
             if (typeof $(container).attr('id') !== 'undefined')
             {
                 var container_id = $(container).attr('id');
                 $.ajax({url: 'deleteethogramcontainer', method: 'POST', data: {container_id: container_id}})
-                        .done(function(msg) {
+                        .done(function (msg) {
                             if (msg.transaction === 'success') {
-                                $(container).fadeOut(1000, function() {
+                                $(container).fadeOut(1000, function () {
                                     $(this.remove())
                                 });
                             }
                         });
             } else {
-                $(container).fadeOut(1000, function() {
+                $(container).fadeOut(1000, function () {
                     $(this.remove())
                 })
             }
         },
-        getContainer: function(event) {
+        getContainer: function (event) {
 
             var container = $(event.target).closest('.' + containerclass);
 
             return container;
+        },
+        getBehaviorsBox: function (event) {
+            console.log(event);
+
+            return $(event.taget).closest('.'.textwrapperclass);
         }
 
     };
 
-    $.fn.ethogram = function(call) {
+    $.fn.ethogram = function (call) {
         if (method[call]) {
             return method[call].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof call === 'object' || !call) {
@@ -293,9 +333,9 @@
             $.error('Method ' + call + ' not found in jQuery.test');
         }
     };
-    $.fn.enterKey = function(fnc) {
-        return this.each(function() {
-            $(this).keypress(function(ev) {
+    $.fn.enterKey = function (fnc) {
+        return this.each(function () {
+            $(this).keypress(function (ev) {
                 var keycode = (ev.keyCode ? ev.keyCode : ev.which);
                 if (keycode == '13') {
                     fnc.call(this, ev);
